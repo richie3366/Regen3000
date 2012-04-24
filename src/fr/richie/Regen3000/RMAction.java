b225be4dfa46c71feee47e116abf03fb14afb1b8
richie3366 <richie3366@richie3366-PC>
2012-04-24 10:53:01 +0200

    Ajout d'un historique du dernier regen + commande "redo"

--- src/fr/richie/Regen3000/RMAction.java
+++ src/fr/richie/Regen3000/RMAction.java
@@ -15,6 +15,8 @@ import com.sk89q.worldedit.regions.Region;
 
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 
+import fr.richie.Regen3000.RMPlugin.RegionLoader;
+
 public class RMAction {
 	
 	public RMPlugin plugin;
@@ -33,11 +35,11 @@ public class RMAction {
 	public BlockVector maxPoint;
 	
 	public int ydiff;
-	
 	public int step = 0;
 	
 	public boolean started = false;
 	public boolean stopped = false;
+	public boolean finished = false;
 	
 	public EditSession es;
 
@@ -45,13 +47,15 @@ public class RMAction {
 	
 	public int runningTask = -1;
 
-	public long startTime = 0;
+	public Long startTime = null;
+	public Long finishTime = null;
 
 	public boolean nolava = false;
-
 	public boolean bcast = false;
 
 	public String period;
+
+	private String historyNode;
 	
 	public RMAction(RMPlugin plugin, ProtectedRegion region, Region weRegion, Player player,
 			World wsource, BlockVector2D chunkMin, BlockVector2D chunkMax,
@@ -86,11 +90,14 @@ public class RMAction {
 		this.started = true;
 		this.startTime = System.currentTimeMillis();
 		
+		historyNode = "regens."+wsource.getName()+"."+region.getId();
+		
+		saveToHistoryFile();
+		
 		String wToCreate = "RMregen_"+region.getId().toLowerCase()+"_"+(new Random().nextInt(1000000));
 		
 		if(bcast){
 			sendMessage(ChatColor.AQUA + "[Regen3000] Régénération de la mine "+region.getId()+" démarrée.");
-			// TODO : message plus parlant ?
 		}
 		
 		
@@ -110,6 +117,24 @@ public class RMAction {
 		this.runningTask = Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new ChunkLoaderRunnable(this));
 	}
 	
+	private void saveToHistoryFile() {
+
+		plugin.getHistoryFile().set(historyNode+".playerName", player.getName());
+		plugin.getHistoryFile().set(historyNode+".period", period);
+		plugin.getHistoryFile().set(historyNode+".wallID", wallBlockMaterial.getId());
+		plugin.getHistoryFile().set(historyNode+".ydiff", ydiff);
+		plugin.getHistoryFile().set(historyNode+".nolava", nolava);
+		plugin.getHistoryFile().set(historyNode+".bcast", bcast);
+		plugin.getHistoryFile().set(historyNode+".started", started);
+		plugin.getHistoryFile().set(historyNode+".startedTime", startTime);
+		plugin.getHistoryFile().set(historyNode+".finished", finished);
+		plugin.getHistoryFile().set(historyNode+".finishTime", finishTime);
+		
+		plugin.saveHistoryFile();
+		
+	}
+
+
 	public void genSourceWorld(){
 		
 		this.step++;
@@ -131,6 +156,11 @@ public class RMAction {
 		
 		if(this.started)
 			RMUtils.removeWorld(this.wsource.getName());
+		
+		finished = true;
+		finishTime = System.currentTimeMillis();
+		
+		saveToHistoryFile();
 		
 		plugin.actionList.remove(player.getName());
 		
@@ -165,6 +195,18 @@ public class RMAction {
 		}else{
 			player.sendMessage(message);
 		}
+		
+	}
+
+
+	public void sendRecap(RegionLoader loadedRegion, String cmd) {
+		String complement = "sans mettre de murs autour.";
+		
+		if(wallBlockMaterial.getId()>0)
+			complement = "en mettant un mur de "+wallBlockMaterial.toString()+" autour.";
+		
+		player.sendMessage(ChatColor.GOLD + "Vous avez l'intention de regénérer la mine contenue par la region WG '"+loadedRegion.WGregion.getId()+"', d'une taille de "+(loadedRegion.sizeX)+"x"+(loadedRegion.sizeY)+"x"+(loadedRegion.sizeZ)+" [yDiff="+ydiff+", no-lava="+nolava+", broadcast="+bcast+"], "+complement);
+		player.sendMessage(ChatColor.AQUA + "Pour lancer le regen, entrez la commande '"+ChatColor.YELLOW+cmd+" start"+ChatColor.AQUA+"'.");
 		
 	}
 	
--- src/fr/richie/Regen3000/RMPlugin.java
+++ src/fr/richie/Regen3000/RMPlugin.java
@@ -1,5 +1,7 @@
 package fr.richie.Regen3000;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 
 import org.bukkit.Bukkit;
@@ -7,6 +9,8 @@ import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -26,6 +30,8 @@ import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 
 public class RMPlugin extends JavaPlugin{
 	
+	private File historyFile;
+	private FileConfiguration history;
 	
 	public WorldEditPlugin worldEdit;
 	public WorldGuardPlugin worldGuard;
@@ -44,11 +50,36 @@ public class RMPlugin extends JavaPlugin{
 		RMUtils.cleanRegenWorlds();
 		
 		initConfig();
+		initHistoryFile();
 		
 		Bukkit.getPluginManager().registerEvents(new RMPluginListener(this), this);
 		
 	}
-
+	
+	private void initHistoryFile(){
+		
+		historyFile = new File(getDataFolder(), "history.yml");
+		history = YamlConfiguration.loadConfiguration(historyFile);
+		
+		saveHistoryFile();
+		
+	}
+	
+	public void saveHistoryFile(){
+		
+		try {
+			history.save(historyFile);
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public FileConfiguration getHistoryFile(){
+		return history;
+	}
+	
 	private void initConfig() {
 		
 		if(!getConfig().isSet("defaultWallBlockID")){
@@ -101,7 +132,7 @@ public class RMPlugin extends JavaPlugin{
 					return true;
 				}
 
-				if(args[0].toLowerCase().startsWith("r")){
+				if(args[0].toLowerCase().startsWith("reg")){
 					
 					if(this.actionList.containsKey(p.getName())){
 						p.sendMessage(ChatColor.RED + "Vous avez déjà entré une commande de regen.");
@@ -211,9 +242,7 @@ public class RMPlugin extends JavaPlugin{
 					
 					String period = null;
 					Long periodInMinutes = null;
-					// TODO : archivage dernier regen même si pas periode, possibilité de redo sans param
 					// TODO : faire le cron min-by-min
-					// TODO : faire l'archivage A LA FIN ou mettre une variable started/finished dans le node
 					
 					if(arg_period != null){
 						
@@ -232,91 +261,56 @@ public class RMPlugin extends JavaPlugin{
 						}
 					}
 					
+					RegionLoader loadedRegion = null;
+					
+					try{
+						loadedRegion = RegionLoader.loadRegion(this, p, args[1]);
+					}catch(Exception ex){
+						p.sendMessage(ChatColor.RED + "Erreur : "+ex.getMessage());
+					}
 					
 					
-					ProtectedRegion region = this.worldGuard.getRegionManager(p.getWorld()).getRegion(args[1]);
-
-					if(region == null){
-						p.sendMessage(ChatColor.RED + "Nom de région invalide !");
-						return true;
-					}
-
-
-					if ((region instanceof ProtectedCuboidRegion)) {
-						ProtectedCuboidRegion cuboid = (ProtectedCuboidRegion)region;
-						Vector pt1 = cuboid.getMinimumPoint();
-						Vector pt2 = cuboid.getMaximumPoint();
-						CuboidSelection selection = new CuboidSelection(p.getWorld(), pt1, pt2);
-						this.worldEdit.setSelection(p, selection);
-					} else if ((region instanceof ProtectedPolygonalRegion)) {
-						ProtectedPolygonalRegion poly2d = (ProtectedPolygonalRegion)region;
-						Polygonal2DSelection selection = new Polygonal2DSelection(p.getWorld(), poly2d.getPoints(), poly2d.getMinimumPoint().getBlockY(), poly2d.getMaximumPoint().getBlockY());
-
-						this.worldEdit.setSelection(p, selection);
-					} else {
-
-						p.sendMessage(ChatColor.RED + "Région invalide à cause de sa forme... Seuls les cuboids et poly sont supportés.");
-						return true;
-
-					}
-
-					LocalSession sess = this.worldEdit.getSession(p);
-
-					Region laregion = null;
-
-					try {
-						laregion = sess.getSelection(sess.getSelectionWorld());
-					} catch (IncompleteRegionException e1) {
-						p.sendMessage(ChatColor.RED + "Erreur 'impossible' numéro 1.");
-						return true;
-					}
-
-					if(laregion==null){
-						p.sendMessage(ChatColor.RED + "Erreur 'impossible' numéro 2.");
-						return true;
-					}
-
-
-					BlockVector minPoint = region.getMinimumPoint();
-					BlockVector maxPoint = region.getMaximumPoint();
-
-
-
-					int xMin = minPoint.getBlockX() < maxPoint.getBlockX() ? minPoint.getBlockX() : maxPoint.getBlockX();
-					int xMax = minPoint.getBlockX() > maxPoint.getBlockX() ? minPoint.getBlockX() : maxPoint.getBlockX();
-
-					int zMin = minPoint.getBlockZ() < maxPoint.getBlockZ() ? minPoint.getBlockZ() : maxPoint.getBlockZ();
-					int zMax = minPoint.getBlockZ() > maxPoint.getBlockZ() ? minPoint.getBlockZ() : maxPoint.getBlockZ();
-
-					int yMin = minPoint.getBlockY() < maxPoint.getBlockY() ? minPoint.getBlockY() : maxPoint.getBlockY();
-					int yMax = minPoint.getBlockY() > maxPoint.getBlockY() ? minPoint.getBlockY() : maxPoint.getBlockY();
-
-
-					if(yMin - ydiff < 0){
+					if(loadedRegion.realMinPoint.getBlockY() - ydiff < 0){
 						p.sendMessage(ChatColor.RED + "Y-Diff ne doit pas être supérieur à la couche la plus basse.");
+						return true;
 					}
 
 
-					BlockVector realMinPoint = new BlockVector(xMin, yMin, zMin);
-					BlockVector realMaxPoint = new BlockVector(xMax, yMax, zMax);
-
-					BlockVector2D chunkMin = new BlockVector2D(xMin >> 4, zMin >> 4);
-					BlockVector2D chunkMax = new BlockVector2D(xMax >> 4, zMax >> 4);
-
-
-					RMAction action = new RMAction(this, region, laregion, p, p.getWorld(), chunkMin, chunkMax, realMinPoint, realMaxPoint, wallBlockMaterial, period, ydiff, nolava, bcast);
+					RMAction action = new RMAction(this, loadedRegion.WGregion, loadedRegion.regionContainer, p, p.getWorld(), loadedRegion.chunkMin, loadedRegion.chunkMax, loadedRegion.realMinPoint, loadedRegion.realMaxPoint, wallBlockMaterial, period, ydiff, nolava, bcast);
 					
-					String complement = "sans mettre de murs autour.";
-					
-					if(action.wallBlockMaterial.getId()>0)
-						complement = "en mettant un mur de "+action.wallBlockMaterial.toString()+" autour.";
-					
-					p.sendMessage(ChatColor.GOLD + "Vous avez l'intention de regénérer la mine contenue par la region WG '"+region.getId()+"', d'une taille de "+(xMax-xMin+1)+"x"+(yMax-yMin+1)+"x"+(zMax-zMin+1)+" [yDiff="+action.ydiff+", no-lava="+nolava+", broadcast="+bcast+"], "+complement);
-					p.sendMessage(ChatColor.AQUA + "Pour lancer le regen, entrez la commande '"+ChatColor.YELLOW+cmd+" start"+ChatColor.AQUA+"'.");
+					action.sendRecap(loadedRegion, cmd);
 					
 					return true;
 				}
 			
+				if(args[0].toLowerCase().startsWith("redo")){
+					
+					String historyNode = "regens."+p.getWorld().getName()+"."+args[1];
+					
+					if(!getHistoryFile().isSet(historyNode)){
+						p.sendMessage(ChatColor.RED + "Cette mine n'a jamais été régénérée !");
+						return true;
+					}
+					
+					Material wallBlockMaterial = Material.getMaterial(getHistoryFile().getInt(historyNode+".wallID", defaultWallBlockMaterial.getId()));
+					String period = getHistoryFile().getString(historyNode+".period");
+					int ydiff =  getHistoryFile().getInt(historyNode+".ydiff", 0);
+					boolean nolava = getHistoryFile().getBoolean(historyNode+".nolava", defaultNoLava);
+					boolean bcast = getHistoryFile().getBoolean(historyNode+".bcast", defaultBroadcast);
+					
+					RegionLoader loadedRegion = null;
+					try{
+						loadedRegion = RegionLoader.loadRegion(this, p, args[1]);
+					}catch(Exception ex){
+						p.sendMessage(ChatColor.RED + "Erreur : "+ex.getMessage());
+					}
+					
+					RMAction action = new RMAction(this, loadedRegion.WGregion, loadedRegion.regionContainer, p, p.getWorld(), loadedRegion.chunkMin, loadedRegion.chunkMax, loadedRegion.realMinPoint, loadedRegion.realMaxPoint, wallBlockMaterial, period, ydiff, nolava, bcast);
+					
+					action.sendRecap(loadedRegion, cmd);
+					
+				}
+				
 				if(args[0].toLowerCase().startsWith("sta")){
 					
 					
@@ -385,6 +379,103 @@ public class RMPlugin extends JavaPlugin{
 		p.sendMessage(ChatColor.RED + cmd + " unschedule <region_name> [PAS ENCORE DISPO!]");
 		p.sendMessage(ChatColor.RED + cmd + " start");
 		p.sendMessage(ChatColor.RED + cmd + " cancel");
+	}
+	
+	
+	public static class RegionLoader{
+				
+		public ProtectedRegion WGregion;
+		public Region regionContainer;
+		public BlockVector2D chunkMin;
+		public BlockVector2D chunkMax;
+		public BlockVector realMinPoint;
+		public BlockVector realMaxPoint;
+		
+		public int sizeX;
+		public int sizeY;
+		public int sizeZ;
+		
+		public RegionLoader(ProtectedRegion WGregion, Region regionContainer, BlockVector2D chunkMin, BlockVector2D chunkMax, BlockVector realMinPoint, BlockVector realMaxPoint){
+			this.WGregion = WGregion;
+			this.regionContainer = regionContainer;
+			this.chunkMin = chunkMin;
+			this.chunkMax = chunkMax;
+			this.realMinPoint = realMinPoint;
+			this.realMaxPoint = realMaxPoint;
+			
+			sizeX = realMaxPoint.getBlockX() - realMinPoint.getBlockX() + 1;
+			sizeY = realMaxPoint.getBlockY() - realMinPoint.getBlockY() + 1;
+			sizeZ = realMaxPoint.getBlockZ() - realMinPoint.getBlockZ() + 1;
+			
+		}
+		
+		static RegionLoader loadRegion(RMPlugin plugin, Player p, String regionParam) throws Exception{
+			ProtectedRegion region = plugin.worldGuard.getRegionManager(p.getWorld()).getRegion(regionParam);
+
+			if(region == null){
+				throw new Exception("Nom de région invalide !");
+			}
+
+
+			if ((region instanceof ProtectedCuboidRegion)) {
+				ProtectedCuboidRegion cuboid = (ProtectedCuboidRegion)region;
+				Vector pt1 = cuboid.getMinimumPoint();
+				Vector pt2 = cuboid.getMaximumPoint();
+				CuboidSelection selection = new CuboidSelection(p.getWorld(), pt1, pt2);
+				plugin.worldEdit.setSelection(p, selection);
+			} else if ((region instanceof ProtectedPolygonalRegion)) {
+				ProtectedPolygonalRegion poly2d = (ProtectedPolygonalRegion)region;
+				Polygonal2DSelection selection = new Polygonal2DSelection(p.getWorld(), poly2d.getPoints(), poly2d.getMinimumPoint().getBlockY(), poly2d.getMaximumPoint().getBlockY());
+
+				plugin.worldEdit.setSelection(p, selection);
+			} else {
+
+				throw new Exception("Région invalide à cause de sa forme... Seuls les cuboids et poly sont supportés.");
+
+			}
+
+			LocalSession sess = plugin.worldEdit.getSession(p);
+
+			Region regionContainer = null;
+
+			try {
+				regionContainer = sess.getSelection(sess.getSelectionWorld());
+			} catch (IncompleteRegionException e1) {
+				throw new Exception("Erreur 'impossible' numéro 1.");
+			}
+
+			if(regionContainer==null){
+				throw new Exception("Erreur 'impossible' numéro 2.");
+			}
+
+
+			BlockVector minPoint = region.getMinimumPoint();
+			BlockVector maxPoint = region.getMaximumPoint();
+
+
+
+			int xMin = minPoint.getBlockX() < maxPoint.getBlockX() ? minPoint.getBlockX() : maxPoint.getBlockX();
+			int xMax = minPoint.getBlockX() > maxPoint.getBlockX() ? minPoint.getBlockX() : maxPoint.getBlockX();
+
+			int zMin = minPoint.getBlockZ() < maxPoint.getBlockZ() ? minPoint.getBlockZ() : maxPoint.getBlockZ();
+			int zMax = minPoint.getBlockZ() > maxPoint.getBlockZ() ? minPoint.getBlockZ() : maxPoint.getBlockZ();
+
+			int yMin = minPoint.getBlockY() < maxPoint.getBlockY() ? minPoint.getBlockY() : maxPoint.getBlockY();
+			int yMax = minPoint.getBlockY() > maxPoint.getBlockY() ? minPoint.getBlockY() : maxPoint.getBlockY();
+
+
+
+
+
+			BlockVector realMinPoint = new BlockVector(xMin, yMin, zMin);
+			BlockVector realMaxPoint = new BlockVector(xMax, yMax, zMax);
+
+			BlockVector2D chunkMin = new BlockVector2D(xMin >> 4, zMin >> 4);
+			BlockVector2D chunkMax = new BlockVector2D(xMax >> 4, zMax >> 4);
+			
+			return new RegionLoader(region, regionContainer, chunkMin, chunkMax, realMinPoint, realMaxPoint);
+		}
+		
 	}
 	
 }
